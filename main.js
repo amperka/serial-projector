94075460766ec0d256b8bfae1932c872f1ef1443
Victor Nakoryakov <nail.xx@gmail.com>
2015-07-07 23:53:52 +0300

    add: show connection status, handle disconnects

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 
+var RETRY_CONNECT_MS = 1000;
+
 Uint8Array.prototype.slice = function(begin, end) {
-    if (typeof begin === 'undefined')
+    if (typeof begin === 'undefined') {
         begin = 0;
-    if (typeof end === 'undefined')
+    }
+
+    if (typeof end === 'undefined') {
         end = Math.max(this.length, begin);
+    }
 
     var result = new Uint8Array(end - begin);
     for (var i = begin; i < end; ++i) {
@@ -35,25 +40,55 @@ function findLineBreak(b) {
 
 var buffer = new Uint8Array(0);
 
-$(function() {
-    var onConnect = function(connectionInfo) {
-        $('h1').text('Connected!');
+function onConnect(connectionInfo) {
+    buffer = new Uint8Array(0);
+    $('.status').addClass('connected');
+}
+
+function setText(txt) {
+    $('h1').text(txt);
+}
+
+function onReceive(receiveInfo) {
+    var data = receiveInfo.data;
+    data = new Uint8Array(data);
+    buffer = catBuffers(buffer, data);
+
+    var lbr = findLineBreak(buffer);
+    if (lbr !== undefined) {
+        var txt = buffer.slice(0, lbr);
+        buffer = buffer.slice(lbr + 1);
+        txt = uintToString(txt);
+        setText(txt);
     }
+}
 
-    function onReceive(receiveInfo) {
-        var data = receiveInfo.data;
-        data = new Uint8Array(data);
-        buffer = catBuffers(buffer, data);
+function retryConnect() {
+    setTimeout(tryConnect, RETRY_CONNECT_MS);
+}
 
-        var lbr = findLineBreak(buffer);
-        if (lbr !== undefined) {
-            var txt = buffer.slice(0, lbr);
-            buffer = buffer.slice(lbr + 1);
-            txt = uintToString(txt);
-            $('h1').text(txt);
+function onReceiveError(info) {
+    setText(info.error);
+    $('.status').removeClass('connected');
+    retryConnect();
+}
+
+function tryConnect() {
+    chrome.serial.getDevices(function(ports) {
+        if (!ports.length) {
+            setText("No device found >:(");
+            retryConnect();
+            return;
         }
-    };
 
-    chrome.serial.connect("/dev/ttyACM0", {bitrate: 9600}, onConnect);
+        var port = ports[0];
+        chrome.serial.connect(port.path, {bitrate: 9600}, onConnect);
+    });
+}
+
+$(function() {
     chrome.serial.onReceive.addListener(onReceive);
+    chrome.serial.onReceiveError.addListener(onReceiveError);
+
+    tryConnect();
 });
